5a279fa2d55498b8fa937febe79871ef9bc7f872
dperrymorrow <dperrymorrow@gmail.com>
2017-11-20 11:05:59 -0800

    removing unused index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import Debugger from "./Debugger.vue";
-if (typeof window !== "undefined" && window.Vue) Vue.component("debugger", Debugger);
-export default Debugger;
+import ObjTree from "./ObjTree.vue";
+
+if (typeof window !== "undefined" && window.Vue) {
+  Vue.component("debugger", Debugger);
+  Vue.component("obj-tree", ObjTree);
+}
